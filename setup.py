7ae53dbde4a5e67335dcf54bd0d4d56d36889cc1
Javier Junquera <javier.junquera.sanchez@protonmail.com>
2018-11-28 12:34:40 +0100

    Version change

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='stalker',
-      version='0.3',
+      version='0.3.1',
       description='',
       url='https://gitlab.com/junquera/stalker',
       author='Javier Junquera Sánchez',
