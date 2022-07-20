8487486dce0602bd45d5dcc522cd96d835a39395
jaamores <jaamores@devanddel.com>
2022-07-20 12:41:50 +0200

    v1.2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='restalker',
-      version='1.2.0',
+      version='1.2.1',
       description="Text analyzer package",
       long_description=long_description,
       long_description_content_type="text/markdown",
@@ -19,6 +19,7 @@ setup(name='restalker',
         'bech32ref',
         'bs4',
         'nltk',
+        'numpy', 
         'rake-nltk'
       ],
       entry_points=dict(
