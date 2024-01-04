fee6097ac98ef7b5d83f05a88b1d4c2e707e238d
Javier Junquera-Sánchez <javier@junquera.io>
2024-01-04 10:44:01 +0100

    2a1a014 Merge branch 'release'

--- regex_gpg.py
+++ regex_gpg.py
@@ -1,11 +1,12 @@
+import sys
 import re
 
-gpg_header = '-----BEGIN PGP PUBLIC KEY BLOCK-----'
-gpg_footer = '-----END PGP PUBLIC KEY BLOCK-----'
+gpg_header = '-----BEGIN PGP (PUBLIC|PRIVATE) KEY BLOCK-----'
+gpg_footer = '-----END PGP (PUBLIC|PRIVATE) KEY BLOCK-----'
 
 gpg_key = "(%s\n(?:.{,64}\n){,128}%s)\n" % (gpg_header, gpg_footer)
 
-with open('alan@foundries.io.asc') as f:
+with open(sys.argv[1]) as f:
     k = f.read()
 
-print(re.match(gpg_key, k))
+print(re.match(gpg_key, k)[1])
