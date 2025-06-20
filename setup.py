5378f68bdab53a28d78b77d6e1991d95265f4ff6
Germán Esteban <german.esteban@byronlabs.io>
2025-06-20 10:08:31 +0200

    v2.0.6

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="restalker",
-    version="2.0.5",
+    version="2.0.6",
     description="Text analyzer package",
     long_description=long_description,
     long_description_content_type="text/markdown",
