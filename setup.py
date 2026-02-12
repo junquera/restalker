bbfc195b984e266765ceb091180fbf894c4fa6d6
Carlos Cilleruelo <carlos.cilleruelo@uah.es>
2026-02-12 09:20:50 +0100

    Updates version to 2.1.1
    Increments the package version number in preparation for a new release.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="restalker",
-    version="2.1.0",
+    version="2.1.1",
     description="Text analyzer package",
     long_description=long_description,
     long_description_content_type="text/markdown",
